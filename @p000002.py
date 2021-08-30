8da1473314860622280d2a10c9afb7aa77ef4917
coursesxxi <77702597+coursesxxi@users.noreply.github.com>
2021-08-30 18:59:35 -0500

    Add files via upload

---
+++ @p000002.py
@@ -0,0 +1,14 @@
+'''
+Develop function
+FirstReverse
+to Invert a string
+----
+Desarrollar la función
+FirstReverse
+para Invertir un string
+'''
+
+def FirstReverse(a_string):
+    return ''
+
+print(FirstReverse(input()))
---
+++ p000002.py
@@ -0,0 +1,6 @@
+# python p000002.py
+
+def FirstReverse(a_string):
+    return a_string[::-1]
+
+print(FirstReverse(input()))
